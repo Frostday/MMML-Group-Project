a7b0889b2a6e508fd11355d51a0c5fd5704cf6a0
Frostday <dhruvgarg01@gmail.com>
2025-02-22 12:25:14 -0500

    updated sample

--- metrics/make_csv.py
+++ metrics/make_csv.py
@@ -1,18 +1,17 @@
 import json
-from pathlib import Path
 import numpy as np
 import pandas as pd
 
-file_path = "test-A/SPIQA_testA.json"
+file_path = "SPIQA_testA.json"
 
 with open(file_path, 'r') as file:
     text = json.load(file)
 
 data = []
-cols = ['paper', 'question', 'answer', 'reference']
+cols = ['paper', 'question', 'answer', 'reference_figure', 'reference_figure_caption']
 for paper in text.keys():
     for question in text[paper]['qa']:
-        data.append([paper, question['question'], question['answer'], question['reference']])
+        data.append([paper, question['question'], question['answer'], question['reference'], text[paper]['all_figures'][question['reference']]['caption']])
 
 df = pd.DataFrame(data, columns=cols)
 df['generated_answer'] = np.nan
