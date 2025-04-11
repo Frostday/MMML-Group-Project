36c0c50cc4d3706a231d8cc6b14bd4abd3ec9485
Frostday <dhruvgarg01@gmail.com>
2025-04-11 11:57:22 -0400

    val dataset

--- metrics/make_csv.py
+++ metrics/make_csv.py
@@ -2,7 +2,8 @@ import json
 import numpy as np
 import pandas as pd
 
-file_path = "SPIQA_testA.json"
+# file_path = "SPIQA_testA.json"
+file_path = "SPIQA_val.json"
 
 with open(file_path, 'r') as file:
     text = json.load(file)
@@ -15,4 +16,5 @@ for paper in text.keys():
 
 df = pd.DataFrame(data, columns=cols)
 df['generated_answer'] = np.nan
-df.to_csv('sample.csv', index=False)
+# df.to_csv('sample.csv', index=False)
+df.to_csv('data/sample_val.csv', index=False)
